c57f35f4fe29d5a7318421fba972fd3ec979b4cb
CleevensCharlemagne <CleevensCharlemagne@gmail.com>
2023-04-03 22:05:36 -0400

    update

--- minesweeper.py
+++ minesweeper.py
@@ -133,7 +133,8 @@ class Sentence():
         Updates internal knowledge representation given the fact that
         a cell is known to be safe.
         """
-        raise NotImplementedError
+        if cell in self.cells:
+            self.cells.remove(cell)
 
 
 class MinesweeperAI():
